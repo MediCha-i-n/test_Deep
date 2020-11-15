d9b79630878eed74695cc7334b2d4533d3d63204
LUCAssYA <42572954+LUCAssYA@users.noreply.github.com>
2020-11-15 17:18:25 +0900

    Update grpc_pb2.py
    9th commit

--- grpc_pb2.py
+++ grpc_pb2.py
@@ -19,7 +19,7 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   syntax='proto3',
   serialized_options=None,
   create_key=_descriptor._internal_create_key,
-  serialized_pb=b'\n\ngrpc.proto\x12\x04grpc\"F\n\rupdateRequest\x12\r\n\x05model\x18\x01 \x01(\x0c\x12\x12\n\nwhole_size\x18\x02 \x01(\x04\x12\x12\n\nbatch_size\x18\x03 \x01(\x04\"\x1b\n\nvalRequest\x12\r\n\x05model\x18\x01 \x01(\x0c\"\x18\n\x08valReply\x12\x0c\n\x04loss\x18\x01 \x01(\x02\"+\n\x0bupdateReply\x12\r\n\x05model\x18\x01 \x01(\x0c\x12\r\n\x05train\x18\x02 \x01(\x08\x32@\n\x07Updater\x12\x35\n\tsendModel\x12\x13.grpc.updateRequest\x1a\x11.grpc.updateReply\"\x00\x32=\n\tValidator\x12\x30\n\nvalidation\x12\x10.grpc.valRequest\x1a\x0e.grpc.valReply\"\x00\x62\x06proto3'
+  serialized_pb=b'\n\ngrpc.proto\x12\x04grpc\"Z\n\rupdateRequest\x12\r\n\x05model\x18\x01 \x01(\x0c\x12\x12\n\nwhole_size\x18\x02 \x01(\x04\x12\x12\n\nbatch_size\x18\x03 \x01(\x04\x12\x12\n\npatch_size\x18\x04 \x01(\x04\"\x1b\n\nvalRequest\x12\r\n\x05model\x18\x01 \x01(\x0c\"\x18\n\x08valReply\x12\x0c\n\x04loss\x18\x01 \x01(\x02\"+\n\x0bupdateReply\x12\r\n\x05model\x18\x01 \x01(\x0c\x12\r\n\x05train\x18\x02 \x01(\x08\x32@\n\x07Updater\x12\x35\n\tsendModel\x12\x13.grpc.updateRequest\x1a\x11.grpc.updateReply\"\x00\x32=\n\tValidator\x12\x30\n\nvalidation\x12\x10.grpc.valRequest\x1a\x0e.grpc.valReply\"\x00\x62\x06proto3'
 )
 
 
@@ -54,6 +54,13 @@ _UPDATEREQUEST = _descriptor.Descriptor(
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
+    _descriptor.FieldDescriptor(
+      name='patch_size', full_name='grpc.updateRequest.patch_size', index=3,
+      number=4, type=4, cpp_type=4, label=1,
+      has_default_value=False, default_value=0,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
   ],
   extensions=[
   ],
@@ -67,7 +74,7 @@ _UPDATEREQUEST = _descriptor.Descriptor(
   oneofs=[
   ],
   serialized_start=20,
-  serialized_end=90,
+  serialized_end=110,
 )
 
 
@@ -98,8 +105,8 @@ _VALREQUEST = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=92,
-  serialized_end=119,
+  serialized_start=112,
+  serialized_end=139,
 )
 
 
@@ -130,8 +137,8 @@ _VALREPLY = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=121,
-  serialized_end=145,
+  serialized_start=141,
+  serialized_end=165,
 )
 
 
@@ -169,8 +176,8 @@ _UPDATEREPLY = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=147,
-  serialized_end=190,
+  serialized_start=167,
+  serialized_end=210,
 )
 
 DESCRIPTOR.message_types_by_name['updateRequest'] = _UPDATEREQUEST
@@ -216,8 +223,8 @@ _UPDATER = _descriptor.ServiceDescriptor(
   index=0,
   serialized_options=None,
   create_key=_descriptor._internal_create_key,
-  serialized_start=192,
-  serialized_end=256,
+  serialized_start=212,
+  serialized_end=276,
   methods=[
   _descriptor.MethodDescriptor(
     name='sendModel',
@@ -242,8 +249,8 @@ _VALIDATOR = _descriptor.ServiceDescriptor(
   index=1,
   serialized_options=None,
   create_key=_descriptor._internal_create_key,
-  serialized_start=258,
-  serialized_end=319,
+  serialized_start=278,
+  serialized_end=339,
   methods=[
   _descriptor.MethodDescriptor(
     name='validation',

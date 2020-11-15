66512394b2bb12b80deb27a3c73559a1d3f961e4
LUCAssYA <42572954+LUCAssYA@users.noreply.github.com>
2020-11-15 17:17:08 +0900

    Update Validator.py
    9th commit

--- Validator.py
+++ Validator.py
@@ -6,19 +6,23 @@ import grpc_pb2_grpc
 import grpc_pb2
 import pickle
 from concurrent import futures
-from Polyp_gen import train_Generator
+from Polyp_gen import Generator
+import Unet
+from tensorflow.keras.optimizers import Adam
 import tensorflow as tf
 
 class Validator(grpc_pb2_grpc.ValidatorServicer):
     def validation(self, request, context):
-        data = [0,0,0,0,0,0]
-        '''
-        validation data 받는 부분
-        data = 
-        '''
+
+        generator = Generator(patch_size=256, batch_size=8, identity='validator')
+
         model = pickle.loads(request.model)
-        loss = model.evaluate(data)
-        return loss
+        model = model_from_json(model)
+        optimizer = Adam(learning_rate=0.001)
+        model.compile(optimizer=optimizer, loss='binary_crossentropy')
+        loss = model.evaluate(generator.generator(), steps = round(generator.patch_length/8))
+        reply = grpc_pb2.valReply(loss = loss)
+        return reply
 
 if __name__ == "__main__":
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=1), options=[
@@ -26,5 +30,6 @@ if __name__ == "__main__":
         ('grpc.max_receive_message_length', 1024 * 1024 * 1024)
     ])
     grpc_pb2_grpc.add_ValidatorServicer_to_server(Validator(), server)
-    server.add_insecure_port("[::]:8888")
+    server.add_insecure_port("localhost:7777")
     server.start()
+    server.wait_for_termination()

0e1e1e828b782dabee66371f79bc1286ba915a7b
LUCAssYA <42572954+LUCAssYA@users.noreply.github.com>
2020-11-15 17:19:19 +0900

    Update predict.py
    9th commit

--- predict.py
+++ predict.py
@@ -1,53 +1,17 @@
 import Unet
 import numpy as np
-from sklearn.feature_extraction.image import extract_patches
-
-def batch(l, n):
-    for i in range(0, l.shape[0], n):
-        yield l[i:i+n, ::]
+import cv2
 def predict(img):
     model = Unet.Unet()
     model.load_weights("best_model.hdf5")
-
-    patch_size = 256
-    stride = patch_size//2
-
     shape = img.shape
-    img = np.pad(img, [(stride//2, stride//2), (stride//2, stride//2), (0, 0)], mode = 'reflect')
-    pad_shape = img.shape
 
-    pad0 = int(np.ceil(pad_shape[0]/patch_size)*patch_size-pad_shape[0])
-    pad1 = int(np.ceil(pad_shape[1]/patch_size)*patch_size-pad_shape[1])
+    img = cv2.resize(img, (256, 256)).reshape(1, 256, 256, 3)
+    output = model.predict(img)
+    output = np.around(output)
 
-    img = np.pad(img, [(0, pad0), (0, pad1), (0, 0)], mode = 'constant')
-    img = extract_patches(img, (patch_size, patch_size, 3), stride)
-    patches_shape = img.shape
+    output = output.reshape(256, 256)
 
-    img = img.reshape(-1, patch_size, patch_size, 3)
+    output = cv2.resize(output, (shape[1], shape[0]), interpolation=cv2.INTER_CUBIC)
 
-    output = np.zeros((0, patch_size, patch_size))
-
-    for batch_arr in batch(img, 8):
-        arr_out = model.predict(batch_arr/255.0)
-
-        arr_out = arr_out.reshape(arr_out[0], arr_out[1], arr_out[2])
-
-        output.append(arr_out, axis = 0)
-    output = output.reshape(patches_shape[0], patches_shape[1], patch_size, patch_size)
-    output = output[:, :, stride//2:-stride//2, stride//2:-stride//2]
-
-    output = np.concatenate(np.concatenate(output, 1), 1)
-
-    output = output[:shape[0], :shape[1]]
-
-    return output
-
-
-
-
-
-
-
-
-
-
+    return output*255
